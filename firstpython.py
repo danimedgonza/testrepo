a7d15c2f33db45bed4226f6767e17d1a381fdbb3
danimedgonza <danimedgonza99@email.com>
2026-04-18 12:19:03 +0100

    mi cambio

--- firstpython.py
+++ firstpython.py
@@ -1,3 +1,9 @@
-#Display the output
+def calcular(factor, *valores, redondear=False, **kwargs):
+    resultado = [v * factor for v in valores]
 
-print("New python file")
+    if redondear:
+        resultado = [round(x, 2) for x in resultado]
+
+    return resultado
+
+calcular(2, 1.212,2,3, redondear=True)
